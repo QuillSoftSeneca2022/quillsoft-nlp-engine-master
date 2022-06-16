ce75c4bae6d0d67d6bfb17aac391ed80bfc4a436
yulque <double.y.r@gmail.com>
2022-06-16 13:55:39 -0400

    Controllers folder updated

--- Controllers/SummarizationController.py
+++ Controllers/SummarizationController.py
@@ -1,6 +1,8 @@
 #region access to parent folder of the solution
 import os
 import sys
+import requests
+import json
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -17,6 +19,8 @@ from Clustering.QClustering import QClustering
 from Summarization.QSummarization import QSummarization
 from DataExtraction.DocumentHelper import DocumentHelper
 from DataExtraction.DocumentExtraction import DocumentExtraction
+from datetime import datetime
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -35,11 +39,13 @@ def upload_file(file):
             DirHelper.CleanDir(uploads_folder)
             file.save(os.path.join(uploads_folder, secure_filename('input_file.tei.xml')))
             cache['filetype'] = 'tei'
+            print('upload file function - tei.xml ')
         else:
             uploads_folder = ConfigHelper.GetValue("UploadsFolder")
             DirHelper.CreateDir(uploads_folder)
             file.save(os.path.join(uploads_folder, secure_filename('input_file.pdf')))
             cache['filetype'] = 'pdf'
+            print('upload file function - pdf ')
     except:
         result = 'error'
 
@@ -51,9 +57,12 @@ def process_file():
         doc = DocumentHelper.GetDocumentFromTEI()
     else:
         path = ConfigHelper.GetValue("UploadsFolder") + 'input_file.pdf'
-        #call text extraction pipeline
+        #call text extraction pipeline + xml tei file is getting done here. 
         doc = DocumentHelper.GetDocument(path)
-
+    #time stamp for generating xml file 
+   
+    #print(" in process_file function - doc")
+    #print(doc)
     #store in cache extracted sentences
     sentences = []
     for block in doc.blocks:
@@ -68,9 +77,11 @@ def process_file():
 
     #store in cache concepts at document level
     cache['phrases'] = qdoc.concepts
+    print(qdoc)
     return qdoc
 
 #endregion
+
 
 #region PDF files
 
@@ -84,18 +95,20 @@ def upload_pdf_file():
 #processes PDF file previously uploaded
 @app.route('/api/process', methods=["GET"])
 def get_by_pdf():
+    # comment for the test 
     qdoc = process_file()
     return qdoc.to_json()
 
 #endregion
 
-#region TEI/XML files
-
 #uploads TEI/XML file
 @app.route('/api/tei', methods = ['POST'])
 def upload_tei_file():
-   if request.method == 'POST':
+    
+    if request.method == 'POST':
         result = upload_file(request.files['file'])
+        print("/api/tei")
+        #print(result)
         return result
 
 #processes TEI/XML file previously uploaded
@@ -112,6 +125,8 @@ def get_cluster():
     sentences = cache['sentences']
     phrases = cache['phrases']
     result = clustering.BuildClusters(sentences, phrases)
+    print("/api/cluster")
+    #print(json.dumps(result))
     return json.dumps(result)
 
 @app.route('/api/sentences', methods=["POST"])
@@ -120,6 +135,7 @@ def get_sentences():
     p = request.form['phrase']
     sentences = cache['sentences']
     result = clustering.GetSentences(p, sentences)
+   
     return json.dumps(result)
 
 #returns phrases at document level
@@ -131,13 +147,19 @@ def get_phrases():
 #returns TEI/XML file
 @app.route('/api/tei', methods=["GET"])
 def get_tei():
+    with open('./test_files/test.tei.xml', 'r') as tei:
+            print("tei read new")
+            return tei.read()
+
+    print("tei url starts", datetime.now())
     temp_folder = ConfigHelper.GetValue("DumpFolder")
     for (dirpath, dirnames, filenames) in os.walk(temp_folder):
             for filename in filenames:
                 if filename.endswith('.tei.xml'):
                     with open(os.sep.join([dirpath, filename]), 'r', encoding="utf8") as tei:
+                        print("tei read ")
+                        #print(tei.read())
                         return tei.read()
-
 
 #region Methods for accesing configuration file
 
